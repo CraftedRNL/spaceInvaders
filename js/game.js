bd1b5e0447b471ef8382553fa74abcbfebdd6b37
CraftedRNL <126Rliu@frhsd.com>
2026-05-15 08:38:34 -0400

    enemy moving + starting bullets

--- js/game.js
+++ js/game.js
@@ -3,13 +3,16 @@ var myImage;
 var myEnemies = [];
 var eWidth = 50;
 var eHeight = 50;
-var stageOne = [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
+var stageOne = [".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "."]
 var position = 0;
 var bullets = [];
 var inertia = .15;
 var speed = .25;
 var bulletSpeed = 5;
 var bulletTrue = true;
+var eDirection = -1;
+var eInterval = 3000;
+var ebullet = 2000;
 
 var image = document.getElementById("ship");
 function startGame() {
@@ -19,23 +22,25 @@ var myGameArea = {
     canvas: document.createElement("canvas"),
     start: function () {
 
-        this.canvas.width = document.getElementById("box").clientWidth;
-        this.canvas.height = document.getElementById("box").clientHeight;
+        this.canvas.width = 1120;
+        this.canvas.height = 700;
         this.canvas.style.borderColor = "black"
         this.canvas.classList.add("mx-auto")
         this.context = this.canvas.getContext("2d");
 
         myCharacter = new component(40, 40, "red", this.canvas.width, this.canvas.height);
         for (let i = 0; i < stageOne.length; i++) {
-            
-            myEnemies.push(new enemy(eWidth, eHeight, "blue", this.canvas.width/8 + position , 50, 3, 1));
-            position +=  (this.canvas.width/12 +eWidth);
-            console.log( this.canvas.width/12)
+
+            myEnemies.push(new enemy(eWidth, eHeight, "blue", this.canvas.width / 12 - eWidth / 2 + position, 10, 2, 1));
+            position += (this.canvas.width / 12);
+            console.log(this.canvas.width / 12)
         }
 
         document.getElementById("box").appendChild(this.canvas);
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
+        this.enemyInterval = setInterval(moveY, eInterval);
+        this.enemyBulletInterval = setInterval(enemyBullet, ebullet);
         window.addEventListener('keydown', function (e) {
 
             myGameArea.keys = (myGameArea.keys || []);
@@ -58,12 +63,13 @@ var myGameArea = {
 
 
 }
-function bullet(width, height, color, x, y) {
+function bullet(width, height, color, x, y, who) {
     this.width = width;
     this.height = height;
     this.x = x + width / 2 + width;
     console.log(this.x)
     this.y = y - 30;
+    this.who = who;
     this.update = function () {
         ctx = myGameArea.context;
         ctx.fillStyle = color;
@@ -80,18 +86,44 @@ function bullet(width, height, color, x, y) {
                 bulletLeft < enemy.x + enemy.width &&
                 bulletBottom > enemy.y &&
                 bulletTop < enemy.y + enemy.height) {
-                enemyArray.splice(index, 1);
+                    if(enemy.health === 0){
+                        enemyArray.splice(index, 1);
+                    }else{
+                        enemy.health--;
+                    }
+                
                 bullets.splice(i, 1)
                 return true;
             }
         });
     };
+    this.playerHit = function (){
+        const bulletTop = this.y;
+        const bulletBottom = this.y + this.height;
+        const bulletLeft = this.x;
+        const bulletRight = this.x + this.width;
+
+            if (bulletRight > myCharacter.x &&
+                bulletLeft < myCharacter.x + myCharacter.width &&
+                bulletBottom > myCharacter.y &&
+                bulletTop < myCharacter.y + myCharacter.height) {
+                    if(myCharacter.health === 0){
+                        myGameArea.stop();
+                    }else{
+                        myCharacter.health--;
+                    }
+                
+                bullets.splice(i, 1)
+                return true;
+            }
+        ;
+    }
 }
 function enemy(width, height, color, x, y, health, damage) {
     this.width = width;
     this.height = height;
-    this.x = (x / 2 - width / 1.5);
-    this.y = y / 2;
+    this.x = x;
+    this.y = y;
     this.health = health;
     this.damage = damage;
     this.speedX = 0;
@@ -103,14 +135,32 @@ function enemy(width, height, color, x, y, health, damage) {
         ctx.fillRect(this.x, this.y, this.width, this.height);
 
     }
+    this.newPos = function () {
+        this.x += 1 * eDirection;
 
+    }
+    this.wallCrash = function () {
+        const farthest = this.x + this.width;
+        const closest = this.x;
+
+        const wallleft = 0;
+        const wallright = myGameArea.canvas.width;
+
+
+        if (wallleft >= closest) {
+            eDirection = 1;
+        }
+        if (wallright <= farthest) {
+            eDirection = -1;
+        }
+    }
 }
 
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
     this.x = (x / 2 - width / 1.5);
-    this.y = y / 2 * 1.5 - height;
+    this.y = y - height*2 ;
     this.speedX = 0;
     this.speedY = 0;
 
@@ -181,18 +231,18 @@ function updateGameArea() {
         } else if (myCharacter.speedX > 0) {
             myCharacter.speedX -= inertia;
         }
-        if (keys[38]) {
-            myCharacter.speedY += -speed;
-        } else if (myCharacter.speedY < 0) {
-            myCharacter.speedY += inertia;
-        }
-        if (keys[40]) {
-            myCharacter.speedY += speed;
-        } else if (myCharacter.speedY > 0) {
-            myCharacter.speedY -= inertia;
-        }
+        // if (keys[38]) {
+        //     myCharacter.speedY += -speed;
+        // } else if (myCharacter.speedY < 0) {
+        //     myCharacter.speedY += inertia;
+        // }
+        // if (keys[40]) {
+        //     myCharacter.speedY += speed;
+        // } else if (myCharacter.speedY > 0) {
+        //     myCharacter.speedY -= inertia;
+        // }
         if (keys[32] && bulletTrue) {
-            bullets.push(new bullet(10, 25, "green", myCharacter.x, myCharacter.y));
+            bullets.push(new bullet(10, 25, "green", myCharacter.x, myCharacter.y, "player"));
             bulletTrue = false;
             setTimeout(() => {
                 bulletTrue = true;
@@ -202,23 +252,44 @@ function updateGameArea() {
 
 
 
-
     for (i = 0; i < bullets.length; i += 1) {
         bullets[i].y += -bulletSpeed;
         bullets[i].update();
         bullets[i].hit(myEnemies)
         if (bullets[i].y < 0) bullets.splice(i, 1);
     }
-    for (i = 0; i < myEnemies.length; i += 1) {
-        myEnemies[i].update();
+    if (myEnemies.length > 0) {
+        myEnemies[myEnemies.length - 1].wallCrash();
+        myEnemies[0].wallCrash();
+        for (i = 0; i < myEnemies.length; i += 1) {
+            myEnemies[i].update();
+            myEnemies[i].newPos();
+
+        }
     }
+
     myCharacter.newPos();
     myCharacter.update();
 
 
 }
 
+function moveY() {
+    for (i = 0; i < myEnemies.length; i += 1) {
+            myEnemies[i].y += 10;
+        }
+}
+function enemyBullet(){
+    var rng = (Math.floor(Math.random()) * 11)
+    bullets.push(new bullet(10, 25, "red", myEnemies[rng].x, myEnemies[rng].y, "player"));
 
+    for (i = 0; i < bullets.length; i += 1) {
+        bullets[i].y += bulletSpeed;
+        bullets[i].update();
+        bullets[i].playerHithit()
+        if (bullets[i].y > 0) bullets.splice(i, 1);
+    }
+}
 function everyinterval(n) {
     if ((myGameArea.frameNo / n) % 1 == 0) { return true; }
     return false;
